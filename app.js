4a538b6425df78185ada66929a583393ec36a0c0
Brij Patel <brij2608@gmail.com>
2026-09-22 19:27:11 +0530

    new

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ connectToDb();
 
 app.use(
     cors({
-        origin: true,
-        credentials: true
+        origin: "https://clg-frontend-temp.vercel.app",
+        credentials: true,
     })
 );
 
